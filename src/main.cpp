320f1e2378ce1da1e61a81af6e94924f7114b961
yoyofr <ymagnien@gmail.com>
2021-02-21 23:38:02 +0100

    v1.0

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,10 @@
 #include <Arduino.h>
 
 #include <Wire.h>
-#include <U8x8lib.h>
+//#include <U8x8lib.h>
+#include <U8g2lib.h>
 
+#define SIMON_DEFAULT_HIGHSCORE 5
 #define SIMON_MAX_SEQUENCE 64
 #define SIMON_NB_PLAY_BUTTONS 6
 char buttons_led_gpio[] PROGMEM={BTN1_LED,BTN2_LED,BTN3_LED,BTN4_LED,BTN5_LED,BTN6_LED};
@@ -30,18 +32,19 @@ char buttons_led_gpio[] PROGMEM={BTN1_LED,BTN2_LED,BTN3_LED,BTN4_LED,BTN5_LED,BT
 #include <LITTLEFS.h>
 #include <SD.h>
 #include "AudioFileSourceFS.h"
-#include "AudioFileSourcePROGMEM.h"
 #include "AudioGeneratorMP3.h"
-#include "AudioGeneratorWAV.h"
 #include "AudioOutputI2S.h"
 
-AudioGeneratorMP3 *mp3;
+AudioGeneratorMP3 *mp3_jingle;
 AudioGeneratorMP3 *mp3_beep;
-AudioFileSourceFS *file;
-AudioFileSourceFS *fileMP3;
+AudioFileSourceFS *file_jingle;
+AudioFileSourceFS *file_beep;
 AudioOutputI2S *out;
 
-U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ 32, /* data=*/ 33, /* reset=*/ U8X8_PIN_NONE);   // OLEDs without Reset of the Display
+//U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ 32, /* data=*/ 33, /* reset=*/ U8X8_PIN_NONE);   // OLEDs without Reset of the Display
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ 32, /* data=*/ 33, /* reset=*/ U8X8_PIN_NONE);   // All Boards without Reset of the Display
+
+
 
 //extern const uint8_t music_mp3_start[] asm("_binary_src_data_music_mp3_start");
 //extern const uint8_t music_mp3_end[] asm("_binary_src_data_music_mp3_end");
@@ -54,12 +57,15 @@ int button_playingBtn;
 
 #define SIMON_MAX_LEVEL 10
 
+char *message_buttonOK[]={"Oui","C'est bien","Bravo","Continue","Pas mal","C'est ca","Tu y es presque","Yes","Super"};
+char *message_buttonKO[]={"Non","Dommage","Perdu","Entraine toi","Bouh!","Et non!","Pas de bol"};
+char *message_nextSequence[]={"Voila la suite","Ca va se corser","Reste concentré","Attention","Tu fatigues ?","Encore un de plus"};
 int simon_gameSpeed[SIMON_MAX_LEVEL]={800,700,600,500,400,300,200,150,100,50};
 
 char player_sequenceTarget[SIMON_MAX_SEQUENCE];
 char player_currentSequenceIndex,player_targetSequenceIndex;
 char player_level,player_turn,player_highscore;
-
+char player_newhighscore;
 
 #define SIMON_WELCOME_ANIM_TIMEOUT 5*1000
 enum {
@@ -85,8 +91,63 @@ SemaphoreHandle_t audioSemaphore;
 void task_audioLoop(void *parameter);
 
 void button_stopCurrent();
-void display_message(char *msg);
+void display_message(char line,char *msg);
+void display_splashScreen();
+void display_clear();
+void display_highscore();
+void display_gameover();
+void display_update();
+void display_clearLeds();
+
 void simon_startGame();
+void simon_jingleNewHighscore();
+void simon_jingleWrongButton();
+
+void simon_jingleNewHighscore() {
+  //wait for any previous mp3 beep sound to end 1st
+  while (mp3_beep) {
+    vTaskDelay(10);
+  }
+  xSemaphoreTake(audioSemaphore, portMAX_DELAY);
+  if (mp3_jingle) {    
+    mp3_jingle->stop();
+    file_jingle->close();
+    delete file_jingle;
+    delete mp3_jingle;
+    file_jingle=NULL;
+    mp3_jingle=NULL;    
+  }
+  file_jingle = new AudioFileSourceFS(LITTLEFS,"/won.mp3");
+  mp3_jingle = new AudioGeneratorMP3();
+  mp3_jingle->begin(file_jingle, out);
+  xSemaphoreGive(audioSemaphore);
+}
+
+void simon_waitForEndJingle() {
+  while (mp3_jingle) {
+    vTaskDelay(1);
+  }
+}
+
+void simon_jingleWrongButton() {
+  //wait for any previous mp3 beep sound to end 1st
+  while (mp3_beep) {
+    vTaskDelay(10);
+  }
+  xSemaphoreTake(audioSemaphore, portMAX_DELAY);
+  if (mp3_jingle) {    
+    mp3_jingle->stop();
+    file_jingle->close();
+    delete file_jingle;
+    delete mp3_jingle;
+    file_jingle=NULL;
+    mp3_jingle=NULL;    
+  }
+  file_jingle = new AudioFileSourceFS(LITTLEFS,"/lost.mp3");
+  mp3_jingle = new AudioGeneratorMP3();
+  mp3_jingle->begin(file_jingle, out);
+  xSemaphoreGive(audioSemaphore);
+}
 
 void set_buttonLED(int btn_index,bool btn_led_on) {
   if ((btn_index<0)||(btn_index>SIMON_NB_PLAY_BUTTONS-1)) return;
@@ -101,15 +162,15 @@ void play_buttonBeep(int btn_index) {
   button_stopCurrent();
   if (mp3_beep) {    
     mp3_beep->stop();
-    file->close();
-    delete file;
+    file_beep->close();
+    delete file_beep;
     delete mp3_beep;
-    file=NULL;
+    file_beep=NULL;
     mp3_beep=NULL;    
   }
-  file = new AudioFileSourceFS(LITTLEFS,buttons_beep_mp3file[btn_index]);// PROGMEM( buttons_beep_data[btn_index], buttons_beep_data_size[btn_index] );
+  file_beep = new AudioFileSourceFS(LITTLEFS,buttons_beep_mp3file[btn_index]);// PROGMEM( buttons_beep_data[btn_index], buttons_beep_data_size[btn_index] );
   mp3_beep = new AudioGeneratorMP3() ;//WAV();
-  mp3_beep->begin(file, out);
+  mp3_beep->begin(file_beep, out);
   xSemaphoreGive(audioSemaphore);
   button_playingBtn=btn_index;
 }
@@ -117,8 +178,9 @@ void play_buttonBeep(int btn_index) {
 void button_playBtn(int btn_index) {
   //Do not repeat same button if already taken into account (wait for end of mp3_beep/led off)
   //if (btn_index==button_playingBtn) return; 
-  set_buttonLED(btn_index,1);
+  
   play_buttonBeep(btn_index);
+  set_buttonLED(btn_index,1);
 }
 
 void button_stopCurrent() {
@@ -139,23 +201,23 @@ void task_audioLoop(void *parameter) {
           mp3_beep->stop();          
         }        
       } else {
-        file->close();
-        delete file;
+        file_beep->close();
+        delete file_beep;
         delete mp3_beep;
-        file=NULL;
+        file_beep=NULL;
         mp3_beep=NULL;
         button_stopCurrent();
       }
     }
-    if (mp3) {
-      if (mp3->isRunning()) {
-        if (!mp3->loop()) mp3->stop();
+    if (mp3_jingle) {
+      if (mp3_jingle->isRunning()) {
+        if (!mp3_jingle->loop()) mp3_jingle->stop();
       } else {
-        fileMP3->close();
-        delete fileMP3;
-        delete mp3;
-        fileMP3=NULL;
-        mp3=NULL;
+        file_jingle->close();
+        delete file_jingle;
+        delete mp3_jingle;
+        file_jingle=NULL;
+        mp3_jingle=NULL;
       }
     }
     xSemaphoreGive(audioSemaphore);
@@ -171,7 +233,7 @@ void setup() {
   esp_random();
 
   button_playingBtn=-1;
-  player_highscore=0;
+  player_highscore=SIMON_DEFAULT_HIGHSCORE;
 
   if(!LITTLEFS.begin(FORMAT_LITTLEFS_IF_FAILED)){
         Serial.println("LITTLEFS Mount Failed");
@@ -193,18 +255,16 @@ void setup() {
   pinMode(BTN6_IN, INPUT_PULLUP);
   pinMode(BTNRESET_IN, INPUT_PULLUP);	
   
-  u8x8.begin();
-  //u8x8.setPowerSave(0);
+  u8g2.begin();
+  //u8g2.setPowerSave(0);
 
   audioLogger = &Serial;
-  out = new AudioOutputI2S(0,0,8,0);
+  out = new AudioOutputI2S(0,0,32,0);
   out->SetPinout(26,27,25);
-  out->SetGain(0.26);
+  out->SetGain(0.1);
   
-  mp3=NULL;
-  //fileMP3 = new AudioFileSourceFS(LITTLEFS, "/music.mp3" );
-  //mp3 = new AudioGeneratorMP3();
-  //mp3->begin(fileMP3, out);
+  mp3_jingle=NULL;
+  mp3_beep=NULL;
   
   audioSemaphore = xSemaphoreCreateMutex();
   xTaskCreatePinnedToCore(
@@ -216,15 +276,17 @@ void setup() {
     &task_audio,  /* Task handle. */
     0); /* Core where the task should run */
 
-  u8x8.setFont(u8x8_font_chroma48medium8_r);
-    
-  display_message("Ok go");
-}
+  u8g2.setFont(u8g2_font_nokiafc22_tf );//u8g2_font_ncenB08_tr);
+  u8g2.setFontMode(0);
 
-void display_clearLeds() {
-  for (int i=0;i<SIMON_NB_PLAY_BUTTONS;i++) {
-    set_buttonLED(i,0);
-  }
+  display_clear();  
+  display_splashScreen();
+  display_update();
+
+  delay(1000);
+  display_clear();
+  display_highscore();
+  display_update();  
 }
 
 void display_welcomeAnim() {
@@ -248,14 +310,45 @@ void display_welcomeAnim() {
   
 }
 
-void display_message(char *msg) {
-  u8x8.clear();
-  u8x8.setCursor(0,0);
-  u8x8.print("SIMONizer");
-  u8x8.setCursor(0,1);
-  u8x8.printf("Highscore: %d",player_highscore);
-  u8x8.setCursor(0,3);
-  u8x8.print(msg);  
+void display_clear() {
+  u8g2.clearBuffer();  
+}
+
+void display_update() {
+  u8g2.sendBuffer();
+}
+
+void display_splashScreen() {
+  u8g2.clearBuffer();  
+  u8g2.drawStr(0,5*7,"SIMONizer");    
+}
+
+void display_gameover() {
+  u8g2.clearBuffer();
+  u8g2.drawStr(2,4*7,"GAME OVER");  
+}
+
+void display_clearLeds() {
+  for (int i=0;i<SIMON_NB_PLAY_BUTTONS;i++) {
+    set_buttonLED(i,0);
+  }
+}
+void display_highscore() {
+  char strtmp[32];
+  u8g2.setCursor(0,0);
+  sprintf(strtmp,"Highscore: %d",player_highscore);   
+  u8g2.drawStr(0,7,strtmp);
+  
+}
+
+void display_message(char line,char *msg) {
+  u8g2.drawStr(0,(line+1)*7,msg);  
+}
+
+void display_score() {
+  char strtmp[32];
+  sprintf(strtmp,"Score: %d",player_targetSequenceIndex-1);
+  u8g2.drawStr(0,7*7,strtmp);
 }
 
 void input_check() {
@@ -276,7 +369,9 @@ void input_check() {
   }
 
   if (digitalRead(BTNRESET_IN)==0) {
-    display_message("BYE BYE!");
+    display_clear();    
+    display_message(3,"BYE BYE!");
+    display_update();
     delay(1000);
     ESP.restart();
   }
@@ -293,10 +388,13 @@ long currentMillis=millis();
     // WELCOME Animation, stop at 1st press or after SIMON_WELCOME_ANIM_TIMEOUT seconds
       if (btn_pressed>=0) {      
         display_clearLeds();
-        button_stopCurrent();        
-        simon_startGame();
         button_stopCurrent();
+        simon_startGame();
         delay(1000);
+
+        display_clear();
+        display_highscore();
+        display_update();
       }
       break;
     case SIMON_MODE_STD_GAME:
@@ -323,7 +421,11 @@ void simon_stdGameTurn(int player_buttonPressed) {
     ////////////////////////////
     //play new sequence    
     ////////////////////////////
-    display_message("Sois attentif");
+    display_clear();
+    display_highscore();
+    display_message(3,message_nextSequence[random(sizeof(message_nextSequence)/sizeof(char*))]);
+    display_score();
+    display_update();
     for (int i=0;i<player_targetSequenceIndex;i++) {      
       button_playBtn(player_sequenceTarget[i]);
       delay(simon_gameSpeed[player_level]);
@@ -335,7 +437,11 @@ void simon_stdGameTurn(int player_buttonPressed) {
     ////////////////////////////////////////////////////////
     player_currentSequenceIndex=0;    
     player_turn=SIMON_GAME_TURN_PLAYER;
-    display_message("A toi de jouer");
+    display_clear();
+    display_highscore();
+    display_message(3,"A ton tour");
+    display_score();
+    display_update();
   } else if (player_turn==SIMON_GAME_TURN_PLAYER) {
     ////////////////////////////////////////////////////////
     //player turn
@@ -353,22 +459,57 @@ void simon_stdGameTurn(int player_buttonPressed) {
         //if expected button, progress in sequence
         ////////////////////////////////////////////////////////
         player_currentSequenceIndex++;
-        if (player_currentSequenceIndex==1) display_message("Oui, continue"); //1st correct button
         if (player_currentSequenceIndex==player_targetSequenceIndex) {
-          if (player_highscore<player_targetSequenceIndex) player_highscore=player_targetSequenceIndex;
-          player_targetSequenceIndex++;
-          
-          player_turn=SIMON_GAME_TURN_SIMON; 
-          display_message("Bravo");
-          delay(1000);
+          if (player_highscore<player_targetSequenceIndex) {
+            player_highscore=player_targetSequenceIndex;
+            player_newhighscore=player_highscore;            
+          }          
+          player_targetSequenceIndex++;          
+          player_turn=SIMON_GAME_TURN_SIMON;          
+          display_clear();
+          display_highscore();
+          display_message(3,message_buttonOK[random(sizeof(message_buttonOK)/sizeof(char*))]);        
+          display_score();
+          display_update();
+          delay(1000);          
         }
       } else {
         ////////////////////////////////////////////////////////
         //if not, game is lost, go back to welcome screen
         ////////////////////////////////////////////////////////                
         simon_mainMode=SIMON_MODE_WELCOME_ANIM;
-        display_message("Perdu");
-        delay(1000);
+        display_clear();
+        display_highscore();
+        display_score();
+        display_message(3,message_buttonKO[random(sizeof(message_buttonKO)/sizeof(char*))]);
+        display_update();
+        button_stopCurrent();
+        simon_jingleWrongButton();
+        delay(300);
+        display_clear();
+        display_highscore();
+        display_score();
+        display_gameover();
+        display_update();
+        delay(700);
+        simon_waitForEndJingle();
+
+        if (player_newhighscore) {
+          display_clear();
+          display_highscore();
+          display_score();
+          display_message(3,"Nouveau highscore!");
+          display_update();
+          simon_jingleNewHighscore();
+          delay(1000);
+          simon_waitForEndJingle();
+        }
+
+
+        /*display_clear();
+        display_highscore();
+        display_score();
+        display_update();*/
       }
     }
   }
@@ -378,12 +519,15 @@ void simon_startGame() {
   simon_mainMode=SIMON_MODE_STD_GAME;
   simon_currentModeElapsed=0;
 
-  display_message("C'est parti");
-  
+  display_clear();
+  display_highscore();
+  display_message(3,"Preparation");  
+  display_update();
   for (int i=0;i<SIMON_MAX_SEQUENCE;i++) player_sequenceTarget[i]=random(SIMON_NB_PLAY_BUTTONS);
   player_currentSequenceIndex=0;        
   player_targetSequenceIndex=1;
   player_level=0;
+  player_newhighscore=0;
   player_turn=SIMON_GAME_TURN_SIMON;
 }
 
@@ -395,11 +539,24 @@ void loop() {
 
   switch (simon_mainMode) {
     case SIMON_MODE_WELCOME_ANIM:
-      display_welcomeAnim();
+      if (!(cnt&7)) display_welcomeAnim();
+      if ((cnt&511)==0) {
+        display_clear();
+        display_highscore();
+        display_message(3,"Appuie pour");
+        display_message(4,"jouer");
+        display_update();
+      } else if ((cnt&511)==400)  {
+        display_clear();
+        display_highscore();
+        display_message(3,"");
+        display_message(4,"");
+        display_update();
+      }
       break;
     default:
       break;
   }
 
-  delay(10);  
+  delay(2);
 }
